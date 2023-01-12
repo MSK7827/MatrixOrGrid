6b5f7c66cbcc9c42a3650b795d7017ced4c54380
Sandeep Mishra <ms063062@gmail.com>
2023-01-12 12:22:54 +0530

    Add project files.

---
+++ MatrixOrGrid/Basics.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixOrGrid
+{
+    public class Basics
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+    }
+}
